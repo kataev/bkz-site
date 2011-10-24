486b6b52a32da5246282a74a28169f1b61b58e62
Катаев Денис <bteamko@gmail.com>
2011-10-24 16:54:42 +0600

    Допиливание стиля

--- priceList/views.py
+++ priceList/views.py
@@ -4,12 +4,4 @@ from priceList.models import *
 
 def price(request):
     p = get_object_or_404(PriceList)
-#    q = p.brick_set.all()
-#    color = ''
-#    for m in range(len(q)-1):
-#        if q[m+1].color.name!=color:
-#            q[m].col+=1
-#        color = q[m].color.name
-#    p.brick = q
-
-    return render(request,'Price.html',{'flatpage':{'content':p,'title':unicode(p)}})
+    return render(request,'Price.html',{'flatpage':{'content':p,'title':unicode(p),'url':'/price/'}})
--- prod/views.py
+++ prod/views.py
@@ -3,5 +3,5 @@ from models import *
 from django.shortcuts import render,get_object_or_404
 
 def products(request):
-    n = {'title':'Продукция','content':Product.objects.all()}
+    n = {'title':'Продукция','content':Product.objects.all(),'url':'/products/'}
     return render(request,'Products.html',{'flatpage':n})
